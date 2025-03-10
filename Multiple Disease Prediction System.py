3590dbe928b6e992cda4be7213beb6e925e212f6
ezekielmose <70557286+ezekielmose@users.noreply.github.com>
2025-03-10 15:38:00 +0300

    Update Multiple Disease Prediction System.py

--- Multiple Disease Prediction System.py
+++ Multiple Disease Prediction System.py
@@ -52,7 +52,97 @@ if (selected == "Healthcare Models"):
     # set title
     st.title ("Hearth Disease Prediction model")
     
+    #getting the input data from the user  
+    age = st.text_input("Enter the Patient's Age 15 - 80")
+    sex = st.text_input("Enter the Patient's Gender (0 [F] or 1[M])")
+    Chest_Pain = st.text_input("Chest Pain level (0,1,2 or 3)")
+    Blood_Pressure= st.text_input("The Blood Pressure(mm Hg)level (94-200) ")
+    cholestoral = st.text_input("Cholestoral Level (mg/dl) (131 -290)")
+    Fasting_Blood_Sugar = st.text_input("Patient's Fasting Blood Sugar (0,1)")
+    resting_electrocardiographic = st.text_input("Electrocardiographic level (0, 1 or 2)")
+    Maximum_Heart_Rate= st.text_input("Maximum Heart Rate (99 - 162)")
+    Excersize_Includes = st.text_input("Enter the Patient's Excersize_Includes")
+    ST_Depression = st.text_input("Patient's ST Depression [ECG or EKG] (0.0 - 4.4)")
+    Slope_of_Excersize	 = st.text_input("Patient's Slope of Excersize (0,1 or 2)")
+    Number_of_vessels = st.text_input("Number of vessels (0, 1,2,3 or 4)")
+    Thalassemia = st.text_input("Thalassemia (1,2,3 or 4)")
+    
+    
+    
     
 if (selected == "Stroke Prediction"):
     # set title
     st.title ("Stroke Prediction model")
+    
+    
+    # Apply custom CSS
+    # Apply custom CSS to style the dropdown field and menu
+    st.markdown("""
+        <style>
+         
+            
+            /* Style the dropdown menu */
+            ul {
+                background-color: smoky black !important;
+            }
+    
+            /* Style dropdown options */
+            li {
+                color: white !important;
+                font-weight: bold;
+            }
+    
+            /* Change the hover effect on dropdown options */
+            li:hover {
+                background-color: grey !important;
+                color: black !important;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+     
+    gender = int(st.selectbox(label = "What is the Gender (0 - Female and 1 - Male)", options = [1, 0]))
+
+    age = st.number_input( "Select the Age using the + and - signs )", 
+        min_value=1, 
+        max_value=80, 
+        step=1
+                                       )
+
+    #age = float(st.text_input("Enter the age", "0"))
+    hypertension = int(st.text_input("Hypertension 0 for -ve and 1 for +ve", "0"))
+    heart_disease = int(st.text_input("Heart disease 0 for has and 1 for not", "0"))
+    ever_married = int(st.text_input("Ever married 0 for No and 1 for Yes", "0"))
+    work_type = int(st.selectbox( label = "Work type 0-private. 1-self-employed, 2-children, 3-gov_job, and 4-never worked", options = [0, 1,2,3,4]))
+    Residence_type = int(st.text_input("Residence type 0 for urban and 1 for rural", "0"))
+
+    avg_glucose_level = st.number_input( "Select the avg_glucose_level using the + and - signs )", 
+        min_value=100, 
+        max_value=300, 
+        step=1
+                                       )
+    
+    #avg_glucose_level = float(st.text_input("Enter any value of avg_glucose_level as per the measurements", "0"))
+
+    bmi = st.number_input( "Select the BMI value", 
+        min_value=10, 
+        max_value=97, 
+        step=1
+    )
+    #bmi = float(st.text_input("Enter any value of BMI as per the measurements", "0"))
+
+    smoking_status = st.number_input( "Smoking status (0: Never smoked, 1: Unknown, 2: Formerly smoked, 3: Smokes)", 
+        min_value=0, 
+        max_value=3, 
+        step=1
+    )
+    
+    #smoking_status = int(st.text_input("Smoking status 0 for never smoked, 1 for unknown, 2 for formerly smoked, 3 for smokes", "0"))
+    
+    if st.button('CLICK HERE TO PREDICT'):
+        makeprediction = stroke_model.predict([[gender, age, hypertension, heart_disease, ever_married, work_type, Residence_type, avg_glucose_level, bmi, smoking_status]])
+        output = round(makeprediction[0])  # Ensure it's an integer (0 or 1)
+    
+        if output == 0:
+            st.success("The patient is **not at risk** of stroke.")
+        else:
+            st.warning("The patient **is at risk** of stroke. Please consult a doctor.") 
